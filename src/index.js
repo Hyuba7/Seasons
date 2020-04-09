9fb7eac0c093d72c9631bfaeafa754a393967c8f
bimalki <abyu629@gmail.com>
2020-04-10 00:47:25 +0900

    border redの枠組まで

--- src/index.js
+++ src/index.js
@@ -1,42 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import	Spinner	from	'./Spinner';
 
 class App extends React.Component {
-	
-	
-	state = { lat: null,	errorMessage:''};
-		
-		
+  state = { lat: null, errorMessage: "" };
 
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
 
-		componentDidMount()	{
-			window.navigator.geolocation.getCurrentPosition(
-				position =>	this.setState({lat:position.coords.latitude}),
-				err => this.setState({errorMessage: err.message})
-				);
-		}
-
-		componentDidUpdate()	{
-			console.log('')
-		}
-	
-	
-	render() {
-		//位置情報が取得できなかった場合
-		if(this.state.errorMessage && !this.state.lat)	{
-		return	<div>エラー:	{this.state.errorMessage}</div>;
-		}
-		//位置情報を取得した場合
-		if(!this.state.errorMessage && this.state.lat)	{
-		return	<SeasonDisplay  lat={this.state.lat}	/>
-		}
+		renderContent	()	{
+			//位置情報が取得できなかった場合
+			if (this.state.errorMessage && !this.state.lat) {
+				return <div>エラー: {this.state.errorMessage}</div>;
+			}
+			//位置情報を取得した場合
+			if (!this.state.errorMessage && this.state.lat) {
+				return <SeasonDisplay lat={this.state.lat} />;
+			}
 			//ローディング画面
-		return	<div>読み込み中</div>
+			return <Spinner	message="位置情報の使用を許可してください"/>;
+		}
 
-	}
+  render() {
+	return <div className="border	red">{this.renderContent()}</div>;
+  }
 }
 
-ReactDOM.render(
-	<App/>, 
-	document.querySelector(`#root`));
+ReactDOM.render(<App />, document.querySelector(`#root`));
